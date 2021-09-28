e2b7f4d375aaf7186b343ef1c83f902c06d6a62b
Matthew Jackson <matt@Matthews-MacBook-Pro.local>
2021-09-28 14:08:24 +0100

    feat: styled the coin form

--- client/components/NewPosition.tsx
+++ client/components/NewPosition.tsx
@@ -10,12 +10,18 @@ import {
   useDisclosure,
   Button, IconButton, FormControl, FormLabel, FormHelperText
 } from "@chakra-ui/react";
-import { FaPlus } from "react-icons/fa";
+import { FaPlus, FaSearch } from "react-icons/fa";
 import { VStack, HStack } from '@chakra-ui/layout';
-import { Input } from '@chakra-ui/input';
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
 
 
 interface INewPositionProps {
@@ -27,8 +33,8 @@ const NewPosition: FunctionComponent<INewPositionProps> = ({ setNewEntry, newEnt
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [symbol, setSymbol] = useState('');
-  const [openPrice, setOpenPrice] = useState('');
-  const [quantity, setQuantity] = useState('');
+  const [openPrice, setOpenPrice] = useState(0);
+  const [quantity, setQuantity] = useState(0);
   const [startDate, setStartDate] = useState(new Date());
 
   function handleSubmit(e:FormEvent) {
@@ -38,25 +44,26 @@ const NewPosition: FunctionComponent<INewPositionProps> = ({ setNewEntry, newEnt
       credentials: "include",
       method: "POST",
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({ symbol, openPrice, quantity, startDate: startDate.getTime() })
+      body: JSON.stringify({ symbol, openPrice, quantity, startDate: startDate.getTime()})
     })
     .then(res => res.json())
     .then(data => {
         onClose();
         setSymbol('');
-        setOpenPrice('');
-        setQuantity('');
+        setOpenPrice(0);
+        setQuantity(0);
         setStartDate(new Date());
         setNewEntry(newEntry + 1);
     })
     .catch(err => console.log('blah',err))
   }
+  const format = (val:number) => `$${val}`
 
   return (
     <>
       <IconButton aria-label="{ icon: Element; }" onClick={onOpen} icon={<FaPlus />} colorScheme='blue' size='sm' />
 
-      <Modal isOpen={isOpen} onClose={() => {onClose(); setSymbol(''); setOpenPrice(''); setQuantity(''); setStartDate(new Date())}} isCentered>
+      <Modal isOpen={isOpen} onClose={() => {onClose(); setSymbol(''); setOpenPrice(0); setQuantity(0); setStartDate(new Date())}} isCentered>
         <ModalOverlay />
         <ModalContent maxW="56rem">
           <ModalHeader>Add position</ModalHeader>
@@ -65,22 +72,37 @@ const NewPosition: FunctionComponent<INewPositionProps> = ({ setNewEntry, newEnt
             
           <form onSubmit={(e) => handleSubmit(e)}>
               <VStack pb='2rem'>
-                <HStack mt='1rem'  borderRadius='0.5rem' spacing={4} pt='15' pb='30' mr={0}>
+                <HStack mt='1rem'  borderRadius='0.5rem' spacing={6} pt='15' pb='30' mr={0}>
                   <FormControl id="symbol" isRequired>
                     <FormLabel>Symbol</FormLabel>
-                    <Input borderColor='teal'type="text" w='10rem'  value={symbol} onChange={(e)=>setSymbol(e.target.value)} />
+                    <InputGroup>
+                    <InputLeftElement pointerEvents="none" children={<FaSearch color="gray.300" />}/>
+                      <Input placeholder='currency' borderColor='teal'type="text" w='11rem'  value={symbol} onChange={(e)=>setSymbol(e.target.value)} />
+                    </ InputGroup>
                   </FormControl>
                   <FormControl id="openPrice" isRequired>
                     <FormLabel>Open price</FormLabel>
-                    <Input w='10rem' type="text" value={openPrice} borderColor='teal' onChange={(e)=>setOpenPrice(e.target.value)}/>
+                    <Input as={NumberInput} variant='unstyled' defaultValue={0} precision={2} step={10} borderColor='teal' w='11rem' value={format(openPrice)} onChange={(value:any)=>setOpenPrice(value)}>
+                      <NumberInputField />
+                      <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                      </NumberInputStepper>
+                    </Input>
                   </FormControl>
                   <FormControl id="quantity" isRequired>
                     <FormLabel>Quantity</FormLabel>
-                    <Input w='10rem' borderColor='teal'type="text"  value={quantity} onChange={(e)=>setQuantity(e.target.value)} />
+                    <Input w='11rem' as={NumberInput} variant='unstyled' defaultValue={0} precision={2} step={5} borderColor='teal' value={quantity} onChange={(value:any)=>setQuantity(value)}>
+                      <NumberInputField />
+                        <NumberInputStepper>
+                          <NumberIncrementStepper />
+                          <NumberDecrementStepper />
+                      </NumberInputStepper>
+                    </Input>
                   </FormControl>
                   <FormControl id="quantity" isRequired>
                     <FormLabel>Open date</FormLabel>
-                    <Input w='10rem' borderColor='teal' as={DatePicker} selected={startDate} onSelect={(date:any) => setStartDate(date)} />
+                    <Input w='11rem' borderColor='teal' as={DatePicker} selected={startDate} onSelect={(date:any) => setStartDate(date)} />
                   </FormControl>
                 </HStack>
                 <Button type='submit' onClick={onClose} w='8rem' colorScheme='teal'>Add</Button>
